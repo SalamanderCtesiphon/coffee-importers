1bb8879615d398832031047d4ae17a2087770ea6
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-15 10:56:29 -0500

    styled for mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,8 @@ function Contact() {
         <h1>Contact</h1>
     <p className='text-card'>Please don't attempt to contact us. This is not a real company. This is just a portfolio project that I am doing in an attempt to get a job.</p>
     <br />
-    <p className='text-card'>Now, if your are a recruiter or a human resources manager please do contact me here:<a href='https://www.linkedin.com/in/shannon-brookshire-817577265/' id='social'> @Linked In </a></p>
+    <p className='text-card'>Now, if your are a recruiter or a human resources manager please do contact me here:</p>
+    <a href='https://www.linkedin.com/in/shannon-brookshire-817577265/' id='social'> @Linked In </a>
    
     </div>
   )
